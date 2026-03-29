bc36864423148e6f9eb82adeedbdc2fe35a7f161
Mohit <mohittoor108@gmail.com>
2026-03-30 05:28:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,21 +1,10 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-        int m = mat.length;
-        int n = mat[0].length;
-        k %= n; 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i % 2 == 0) {
-                    if (mat[i][j] != mat[i][(j + k) % n]) {
-                        return false;
-                    }
-                } else {
-                    if (mat[i][j] != mat[i][(j - k + n) % n]) {
-                        return false;
-                    }
-                }
+        k %= mat[0].length;
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[0].length;j++){
+                
             }
         }
-        return true;
     }
 }
