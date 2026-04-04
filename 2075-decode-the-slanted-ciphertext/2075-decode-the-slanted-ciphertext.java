33ba5696940004380c67888a7f8cd0e9fe35603c
Mohit <mohittoor108@gmail.com>
2026-04-05 00:17:10 +0530

    Time: 18 ms (97.63%), Space: 56.8 MB (98.22%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.java
@@ -1,25 +1,49 @@
+// class Solution {
+//     public String decodeCiphertext(String encodedText, int rows) {
+//         int n = encodedText.length();
+//         int cols = n/rows;
+//         char[][] matrix = new char[rows][cols];
+//         int c = 0;
+//         for(int i = 0;i<rows;i++){
+//             for(int j = 0;j<cols;j++){
+//                 matrix[i][j] = encodedText.charAt(c++);
+//             }
+//         }
+//         StringBuilder result = new StringBuilder();
+//         for(int i = 0;i<cols;i++){
+//             int j = 0,k=i;
+//             while(j<rows && k<cols){
+//                 result.append(matrix[j++][k++]);
+//             }
+//         }
+//         int end = result.length()-1;
+//         while(end >= 0 && result.charAt(end) == ' '){
+//             end--;
+//         }
+//         return result.substring(0,end+1);
+//     }
+// }
+
+
 class Solution {
     public String decodeCiphertext(String encodedText, int rows) {
+        if (rows == 1)
+            return encodedText;
         int n = encodedText.length();
-        int cols = n/rows;
-        char[][] matrix = new char[rows][cols];
-        int c = 0;
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<cols;j++){
-                matrix[i][j] = encodedText.charAt(c++);
+        int cols = n / rows;
+        StringBuilder res = new StringBuilder(n);
+        for (int c = 0; c < cols; c++) {
+            int r = 0, j = c;
+            while (r < rows && j < cols) {
+                res.append(encodedText.charAt(r * cols + j));
+                r++;
+                j++;
             }
         }
-        StringBuilder result = new StringBuilder();
-        for(int i = 0;i<cols;i++){
-            int j = 0,k=i;
-            while(j<rows && k<cols){
-                result.append(matrix[j++][k++]);
-            }
-        }
-        int end = result.length()-1;
-        while(end >= 0 && result.charAt(end) == ' '){
+        int end = res.length() - 1;
+        while (end >= 0 && res.charAt(end) == ' ') {
             end--;
         }
-        return result.substring(0,end+1);
+        return res.substring(0, end + 1);
     }
 }
