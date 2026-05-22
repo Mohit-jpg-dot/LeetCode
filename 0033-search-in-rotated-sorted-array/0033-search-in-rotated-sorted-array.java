b2b158576d3788c17c0cfeb0c4e27ee28ff5ab39
Mohit <mohittoor108@gmail.com>
2026-05-23 04:33:20 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,50 @@
 class Solution {
-    public static int rotatedBinarySearch(int[] arr, int target, int start, int end){
+    // public static int rotatedBinarySearch(int[] arr, int target, int start, int end){
+    //     if(start > end) return -1;
+    //     int mid = start + (end - start)/2;
+    //     if(arr[mid] == target){
+    //         return mid;
+    //     }
+    //     if(arr[start] <= arr[mid]){
+
+    //         if(target >= arr[start] && target < arr[mid]){
+    //             return rotatedBinarySearch(arr, target, start, mid - 1);
+    //         }
+    //         else{
+    //             return rotatedBinarySearch(arr, target, mid + 1, end);
+    //         }
+    //     }
+    //     else{
+
+    //         if(target > arr[mid] && target <= arr[end]){
+    //             return rotatedBinarySearch(arr, target, mid + 1, end);
+    //         }
+    //         else{
+    //             return rotatedBinarySearch(arr, target, start, mid - 1);
+    //         }
+    //     }
+    // }
+
+    public int rotatedBinarySearch(int[] arr,int target,int start,int end){
         if(start > end) return -1;
         int mid = start + (end - start)/2;
         if(arr[mid] == target){
             return mid;
         }
         if(arr[start] <= arr[mid]){
-
             if(target >= arr[start] && target < arr[mid]){
-                return rotatedBinarySearch(arr, target, start, mid - 1);
+                return rotatedBinarySearch(arr,target,start,mid-1);
             }
             else{
-                return rotatedBinarySearch(arr, target, mid + 1, end);
+                return rotatedBinarySearch(arr,target,mid+1,end);
             }
         }
         else{
-
-            if(target > arr[mid] && target <= arr[end]){
-                return rotatedBinarySearch(arr, target, mid + 1, end);
+            if(target >= arr[mid] && target <= arr[end]){
+                    return rotatedBinarySearch(arr,target,mid+1,end);
             }
             else{
-                return rotatedBinarySearch(arr, target, start, mid - 1);
+                    return rotatedBinarySearch(arr,target,start,mid-1);
             }
         }
     }
