057004b08c4d50fff9df581e524fd8442b00a8d2
Mohit <mohittoor108@gmail.com>
2026-05-30 03:30:12 +0530

    Time: 1 ms (100%), Space: 44.6 MB (94.12%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public int digitSum(int n){
+//         int result = 0;
+//         String res = String.valueOf(n);
+//         for(char x:res.toCharArray()){
+//             result += x - '0';
+//         }
+//         return result;
+//     }
+//     public int minElement(int[] nums) {
+//         for(int i = 0;i<nums.length;i++){
+//             String str = String.valueOf(nums[i]);
+//             if(str.length() > 1){
+//                 nums[i] = digitSum(nums[i]);
+//             }
+//         }
+//         int min = Integer.MAX_VALUE;
+//         for(int x:nums){
+//             min = Math.min(min,x);
+//         }
+//         return min;
+//     }
+// }
+
 class Solution {
-    public int digitSum(int n){
-        int result = 0;
-        String res = String.valueOf(n);
-        for(char x:res.toCharArray()){
-            result += x - '0';
+    private int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
         }
-        return result;
+        return sum;
     }
+
     public int minElement(int[] nums) {
-        for(int i = 0;i<nums.length;i++){
-            String str = String.valueOf(nums[i]);
-            if(str.length() > 1){
-                nums[i] = digitSum(nums[i]);
-            }
-        }
         int min = Integer.MAX_VALUE;
-        for(int x:nums){
-            min = Math.min(min,x);
+        for (int num : nums) {
+            min = Math.min(min, digitSum(num));
         }
         return min;
     }
