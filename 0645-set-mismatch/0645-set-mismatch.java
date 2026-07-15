efaba9ff6f663005593c31d4bfc2566af5bcb78d
Mohit <mohittoor108@gmail.com>
2026-07-16 02:16:17 +0530

    Time: 2 ms (94.65%), Space: 47.6 MB (65.28%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -3,61 +3,48 @@
 
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // int n = nums.length;
-        // int xorAll = 0;
-        // int xorArray = 0;
-
-        // for (int i = 1; i <= n; i++) {
-        //     xorAll ^= i;
-        // }
-
-        // for (int num : nums) {
-        //     xorArray ^= num;
-        // }
-
-        // int xorResult = xorArray ^ xorAll;
-
-        // int rightmostSetBit = xorResult & -xorResult;
-
-        // int xorSet = 0;
-        // int xorNotSet = 0;
-
-        // for (int i = 1; i <= n; i++) {
-        //     if ((i & rightmostSetBit) != 0) {
-        //         xorSet ^= i;
-        //     } else {
-        //         xorNotSet ^= i;
-        //     }
-        // }
-        // for (int num : nums) {
-        //     if ((num & rightmostSetBit) != 0) {
-        //         xorSet ^= num;
-        //     } else {
-        //         xorNotSet ^= num;
-        //     }
-        // }
-        // for (int num : nums) {
-        //     if (num == xorSet) {
-        //         return new int[]{xorSet, xorNotSet};
-        //     }
-        // }
-        // return new int[]{xorNotSet, xorSet};
-
-
         int n = nums.length;
-        int expected=(n*(n+1))/2;
-        int missing = 0;
-        int actual = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int x:nums){
-            if(set.add(x) == false){
-                missing = x;
-            }
-            else{
-                actual += x;
+        int xorAll = 0;
+        int xorArray = 0;
+
+        for (int i = 1; i <= n; i++) {
+            xorAll ^= i;
+        }
+
+        for (int num : nums) {
+            xorArray ^= num;
+        }
+
+        int xorResult = xorArray ^ xorAll;
+
+        int rightmostSetBit = xorResult & -xorResult;
+
+        int xorSet = 0;
+        int xorNotSet = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & rightmostSetBit) != 0) {
+                xorSet ^= i;
+            } else {
+                xorNotSet ^= i;
             }
         }
-        return new int[]{missing,expected - actual};
+
+        for (int num : nums) {
+            if ((num & rightmostSetBit) != 0) {
+                xorSet ^= num;
+            } else {
+                xorNotSet ^= num;
+            }
+        }
+
+        for (int num : nums) {
+            if (num == xorSet) {
+                return new int[]{xorSet, xorNotSet};
+            }
+        }
+
+        return new int[]{xorNotSet, xorSet};
     }
 }
 
