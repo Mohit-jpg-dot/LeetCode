08b14d82829af9b9ed1cc02dc50c3f59656ae7fa
Mohit <mohittoor108@gmail.com>
2026-08-19 18:42:58 +0530

    Time: 33 ms (74.48%), Space: 47.8 MB (86.27%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -1,11 +1,25 @@
 class Solution {
     public String makeFancyString(String s) {
-        int cnt = 0;
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0;i<s.length();i++){
+        //     sb.append(s.charAt(i));
+        //     if(s.length() > i+2 && s.charAt(i) == s.charAt(i+1) && s.charAt(i) == s.charAt(i+2)){
+        //         sb.deleteCharAt(sb.length()-1);
+        //     }
+        // }
+        // return sb.toString();
+
+        int cnt = 1;
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i<s.length();i++){
-            sb.append(s.charAt(i));
-            if(s.length() > i+2 && s.charAt(i) == s.charAt(i+1) && s.charAt(i) == s.charAt(i+2)){
-                sb.deleteCharAt(sb.length()-1);
+            if(s.length() > i+1 && s.charAt(i) == s.charAt(i+1)){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+            if(cnt <= 2){
+                sb.append(s.charAt(i));
             }
         }
         return sb.toString();
