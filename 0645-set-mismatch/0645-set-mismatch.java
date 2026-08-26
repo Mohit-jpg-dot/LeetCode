683f812f5749bab0290be0ab6865b1e906100210
Mohit <mohittoor108@gmail.com>
2026-08-26 16:37:39 +0530

    Time: 2 ms (94.46%), Space: 47.7 MB (48.16%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,85 +1,38 @@
-
-
-
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // int n = nums.length;
-        // int xorAll = 0;
-        // int xorArray = 0;
-
-        // for (int i = 1; i <= n; i++) {
-        //     xorAll ^= i;
-        // }
-
-        // for (int num : nums) {
-        //     xorArray ^= num;
-        // }
-
-        // int xorResult = xorArray ^ xorAll;
-
-        // int rightmostSetBit = xorResult & -xorResult;
-
-        // int xorSet = 0;
-        // int xorNotSet = 0;
-
-        // for (int i = 1; i <= n; i++) {
-        //     if ((i & rightmostSetBit) != 0) {
-        //         xorSet ^= i;
-        //     } else {
-        //         xorNotSet ^= i;
-        //     }
-        // }
-        // for (int num : nums) {
-        //     if ((num & rightmostSetBit) != 0) {
-        //         xorSet ^= num;
-        //     } else {
-        //         xorNotSet ^= num;
-        //     }
-        // }
-        // for (int num : nums) {
-        //     if (num == xorSet) {
-        //         return new int[]{xorSet, xorNotSet};
-        //     }
-        // }
-        // return new int[]{xorNotSet, xorSet};
-
-
-        // int n = nums.length;
-        // int expected=(n*(n+1))/2;
-        // int missing = 0;
-        // int actual = 0;
-        // Set<Integer> set = new HashSet<>();
-        // for(int x:nums){
-        //     if(set.add(x) == false){
-        //         missing = x;
-        //     }
-        //     else{
-        //         actual += x;
-        //     }
-        // }
-        // return new int[]{missing,expected - actual};
-        
-        return soln(nums);
-    }
-        private static int[] soln(int[] nums) {
-        int[] ans = new int[2];
-        int toalSum = (nums.length * (nums.length + 1)) / 2;
-        int currentSum = 0;
-
-        for (int j : nums) {
-            int absValue = Math.abs(j);
-            int index = absValue - 1;
-            if (nums[index] < 0) {
-                ans[0] = absValue;
-            } else {
-                nums[index] = nums[index] * -1;
-            }
-            currentSum = currentSum + absValue;
+        int xorArray = 0;
+        int xorAll = 0;
+        for(int x:nums){
+            xorArray ^= x;
         }
-
-        ans[1] = (toalSum - currentSum) + ans[0];
-        return ans;
+        for(int i = 1;i<=nums.length;i++){
+            xorAll ^= i;
+        }
+        int xorResult = xorArray ^ xorAll;
+        int rightMostBit = (xorResult & -xorResult);
+        int xorSet = 0;
+        int xorNotSet = 0;
+        for(int i = 1;i<=nums.length;i++){
+            if((rightMostBit&i) != 0){
+                xorSet ^= i;
+            }
+            else{
+                xorNotSet ^= i;
+            }
+        }
+        for(int x:nums){
+            if((x&rightMostBit) != 0){
+                xorSet ^= x;
+            }
+            else{
+                xorNotSet ^= x;
+            }
+        }
+        for(int x:nums){
+            if(x == xorSet){
+                return new int[] {xorSet,xorNotSet};
+            }
+        }
+        return new int[] {xorNotSet,xorSet};
     }
 }
-
-
